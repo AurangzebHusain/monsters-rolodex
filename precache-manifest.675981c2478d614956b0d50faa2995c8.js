6354d81890c62da97fe308874073484898e18d66
Aurangzeb Husain <aurangzebhusain786@gmail.com>
2020-05-28 20:00:44 +0530

    Updates

---
+++ precache-manifest.675981c2478d614956b0d50faa2995c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e83722edb18157163964ac84ac9acd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8ef849b033749ed9dcd6",
+    "url": "/monsters-rolodex/static/css/main.5b144bb2.chunk.css"
+  },
+  {
+    "revision": "d947c89efff2b34f7f9b",
+    "url": "/monsters-rolodex/static/js/2.32fc45e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.32fc45e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef849b033749ed9dcd6",
+    "url": "/monsters-rolodex/static/js/main.3a6ea3a4.chunk.js"
+  },
+  {
+    "revision": "7898c8dcc6a3810f2be0",
+    "url": "/monsters-rolodex/static/js/runtime-main.fcde4e1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
